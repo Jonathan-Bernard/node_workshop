f223ab00693c66a9ae0216a55ddddc4ee144c902
Jonathan-Bernard <johnnyboy59000@gmail.com>
2023-10-03 11:04:41 +0200

    test

---
+++ index.js
@@ -0,0 +1,9 @@
+import chalk from "chalk";
+
+console.log(
+    ("Coucou ") +
+    chalk.blue("Baptiste") + (", ") +
+    chalk.red("Doriane") + (", ") +
+    chalk.green("Tristant") + (" et ") +
+    chalk.white("Raphael !")
+);
